62d4717da76989cf6afdcf45136630f540a7db85
Arthaud <arthaudp33@gmail.com>
2023-01-28 19:44:24 +0100

    feat: remove homepage and redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::redirect('/', '/dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
