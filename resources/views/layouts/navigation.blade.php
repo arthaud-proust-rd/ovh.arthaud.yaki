222a2e1701b8e9112de5c01e5cf621081bc5bc80
Arthaud <arthaudp33@gmail.com>
2023-01-29 18:14:21 +0100

    fix: default week link on mobile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -82,6 +82,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('default-week')" :active="request()->routeIs('default-week')">
+                Semaine type
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
